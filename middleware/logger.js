a2f7269550e2fe9c06e9435e11f7a43a0a42bb4f
Diana Kamensky <dianakamensky@gmail.com>
2022-12-26 21:11:22 +0200

    add winston level

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,28 +3,26 @@ const expressWinston = require("express-winston");
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: "request.log" }),
-    new winston.transports.Console(),
+    new winston.transports.File({ filename: "request.log", level: "debug" }),
+    new winston.transports.Console({ level: "debug" }),
   ],
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.json()
   ),
   meta: true,
-  level: "debug"
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: "error.log" }),
-    new winston.transports.Console(),
+    new winston.transports.File({ filename: "error.log", level: "debug" }),
+    new winston.transports.Console({ level: "debug" }),
   ],
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.json()
   ),
   meta: true,
-  level: "debug"
 });
 
 module.exports = {
